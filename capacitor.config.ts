73ef06434740854d532994cbee33a44a682f385d
Souhail Bektachi <souhalshiv@gmail.com>
2025-04-20 19:12:35 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '"عييت" — Take a Break App',
+  webDir: 'www'
+};
+
+export default config;
